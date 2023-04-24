db4a4e5c0735a43efa2910e0e05cd06b459bc022
mari <mari.17ymy@gmail.com>
2023-04-23 20:53:58 -0400

    arreglos docente

--- src/components/Rutas.jsx
+++ src/components/Rutas.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Docente from './Docente'
-import Docente from './Docente'
 import Estudiante from './Estudiante'
 
 const Rutas = () => {
     return (
         <>
             <Routes>
-                <Route exact path='/Docente' element={<Docente />} />
                 <Route exact path='/Docente' element={<Docente />} />
                 <Route exact path='/Estudiante' element={<Estudiante />} />
             </Routes>
