24cab3bcd38943b7297c259050d8f77c5b6add91
mari <mari.17ymy@gmail.com>
2023-04-24 00:03:14 -0400

    arreglo heroku apiurl

--- src/api/UsuariosApi.js
+++ src/api/UsuariosApi.js
@@ -1,3 +1,3 @@
-export const Apiurl = "http://localhost:5000/api/"
-// export const Apiurl = "https://mysterious-savannah-51630.herokuapp.com/api/"
+// export const Apiurl = "http://localhost:5000/api/"
+export const Apiurl = "https://mysterious-savannah-51630.herokuapp.com/api/"
 
